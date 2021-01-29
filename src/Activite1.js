f90c2d40a2e4e96c7cd62748b52b019e0fcacdc0
blacol <anthonycolvil@gmail.com>
2021-01-29 19:08:51 +0100

    maj

--- src/Activite1.js
+++ src/Activite1.js
@@ -1,8 +1,9 @@
-function Activite1(){
+function Activite1(props){
     return(
         <div>
             <h1>Activite 1</h1> 
-            <span>salut  </span>
+            <span>salut {props.nom_variable} </span>
+            <div>{props.nom_variable2}</div>
 
         </div>
     );
--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Activite1 from './Activite1'
-import Activite2 from './Activite2';
+import Activite1 from './Activite1';
+//import Activite2 from './Activite2';
+//import Activite3 from './Activite3';
 
 function App() {
   return (
     <div>
-      <Activite1 />
-      <Activite2 ma_variable = "toto"/>
+      <Activite1 nom_variable="yolo" nom_variable2="mdr"/>
+      {
+      // {0==1 && (<Activite2 ma_variable = "toto"/> )}\
+       // {0==) && (<Activite3 /> )}
+      }
     </div>
   );
 }
